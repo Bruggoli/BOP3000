8462a1da436c8e7edfde774d6f5358b122789089
Adrian Indergård Ro <oldpopcorn12161@gmail.com>
2025-03-07 15:26:04 +0100

    oppdatert database
    la til flere funksjoner i database, bli med i klubber, lage klubber

--- ts-server/src/models/Klubber.ts
+++ ts-server/src/models/Klubber.ts
@@ -4,7 +4,6 @@ export interface Klubb {
     _id?: ObjectId;
     navn: string;
     beskrivelse: string;
-    admin: string;
-    medlemmer: string[];
+    admin: ObjectId; // Admin er en referanse til bruker-ID
+    medlemmer: { brukerId: ObjectId; rolle: "Admin" | "Moderator" | "Medlem" }[];
 }
-
